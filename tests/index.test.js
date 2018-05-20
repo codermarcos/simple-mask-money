26b529430e13ecffcc562f80522a9861f9c4fb67
codermarcos <marcosbmx.jr@gmail.com>
2018-05-20 02:08:36 -0300

    correct tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -22,11 +22,11 @@ describe('Index', () => {
 
     it('formatToNumber', () => {
       assert.equal(index.formatToNumber('a'), 0);
-      assert.equal(index.formatToNumber('0010'), 0.10);
+      assert.equal(index.formatToNumber('0010'), 0.1);
       assert.equal(index.formatToNumber('a10a1'), 1.01);
-      assert.equal(index.formatToNumber('2.500.025,10'), 2500025.10);
-      assert.equal(index.formatToNumber('a1a.5b0-0.0*25+10'), 1500025.10);
-      assert.equal(index.formatToNumber('1.500.025,10'), 1500025.10);
+      assert.equal(index.formatToNumber('2.500.025,10'), 2500025.1);
+      assert.equal(index.formatToNumber('a1a.5b0-0.0*25+10'), 1500025.1);
+      assert.equal(index.formatToNumber('1.500.025,10'), 1500025.1);
     });    
     
     it('setMask', () => {
@@ -47,15 +47,15 @@ describe('Index', () => {
 
       write(input, '2.500.02.510');
       assert.equal(input.value, '2.500.025,10');
-      assert.equal(input.formatToNumber(), 2500025.10);      
+      assert.equal(input.formatToNumber(), 2500025.1);      
 
       write(input, 'a1a.5b0-0.0*25+10');
       assert.equal(input.value, '1.500.025,10');
-      assert.equal(input.formatToNumber(), 1500025.10);
+      assert.equal(input.formatToNumber(), 1500025.1);
 
       write(input, '1.500.025,10');
       assert.equal(input.value, '1.500.025,10');
-      assert.equal(index.formatToNumber(), 1500025.10);
+      assert.equal(input.formatToNumber(), 1500025.10);
     });
   });
 
@@ -84,8 +84,8 @@ describe('Index', () => {
       assert.equal(index.formatToNumber('R$_.___.'), 0);
       assert.equal(index.formatToNumber('R$_.001.'), 0.001);
       assert.equal(index.formatToNumber('R$_.101.'), 0.101);
-      assert.equal(index.formatToNumber('R$250,002.510.'), 250002.510);
-      assert.equal(index.formatToNumber('R$150,002.510.'), 150002.510);
+      assert.equal(index.formatToNumber('R$250,002.510.'), 250002.51);
+      assert.equal(index.formatToNumber('R$150,002.510.'), 150002.51);
     }); 
     
     it('setMask', () => {
@@ -98,23 +98,23 @@ describe('Index', () => {
       
       write(input, '0010');
       assert.equal(input.value, 'R$_.001.');
-      assert.equal(input.formatToNumber(), 0.10);      
+      assert.equal(input.formatToNumber(), 0.001);      
       
       write(input, 'a10a1');
-      assert.equal(input.value, '1,01');
-      assert.equal(input.formatToNumber(), 1.01);      
+      assert.equal(input.value, 'R$_._10.');
+      assert.equal(input.formatToNumber(), 0.01);      
 
       write(input, '2.500.02.510');
-      assert.equal(input.value, '2.500.025,10');
-      assert.equal(input.formatToNumber(), 2500025.10);      
+      assert.equal(input.value, 'R$25,000,251.000.');
+      assert.equal(input.formatToNumber(), 25000251);      
 
       write(input, 'a1a.5b0-0.0*25+10');
-      assert.equal(input.value, '1.500.025,10');
-      assert.equal(input.formatToNumber(), 1500025.10);
+      assert.equal(input.value, 'R$15,000,251.000.');
+      assert.equal(input.formatToNumber(), 15000251);
 
       write(input, '1.500.025,10');
-      assert.equal(input.value, '1.500.025,10');
-      assert.equal(index.formatToNumber(), 1500025.10);
+      assert.equal(input.value, 'R$15,000,251.000.');
+      assert.equal(input.formatToNumber(), 15000251);
     });
   });
 });
