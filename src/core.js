d494776abf8871ff8996426a10c6f7e990da9e4c
codermarcos <marcosbmx.jr@gmail.com>
2018-09-07 03:37:46 -0300

    correct lint errors

--- src/core.js
+++ src/core.js
@@ -170,7 +170,7 @@ module.exports = class Core {
   numberToText(input) {
     let retorno = this.emptyOrInvalid();
 
-    if (parseFloat(input) !== 'NaN') {
+    if (isNaN(parseFloat(value))) {
       if (input.length <= this.args.fractionDigits) {
         retorno = this.formatDecimal(
           input,
