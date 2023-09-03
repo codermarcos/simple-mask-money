86f48bed595f8b81229f81a30a11219241daf56b
codermarcos <coder.marcos@gmail.com>
2023-09-04 02:45:03 +1000

    🔧 configure test jest On branch main Changes to be committed: 	new file:   jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^src(.*)$': '<rootDir>/src$1',
+  } 
+};
