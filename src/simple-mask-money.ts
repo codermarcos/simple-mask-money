eaee0415be737fd5827ffa2fa9a0055c761eda57
codermarcos <coder.marcos@gmail.com>
2023-09-14 05:23:06 +1000

    🎨 improve create instace to keep tree-shaking On branch main Changes to be committed: 	new file:   src/create-instance-of.ts 	deleted:    src/create-instance.ts 	modified:   src/simple-mask-money.ts

--- src/simple-mask-money.ts
+++ src/simple-mask-money.ts
@@ -1,5 +1,5 @@
 export { default as setMask } from 'src/set-mask';
 export { default as removeMask } from 'src/remove-mask';
 export { default as formatToNumber } from 'src/format-to-number';
-export { default as createInstance } from 'src/create-instance';
+export { default as createInstanceOf } from 'src/create-instance-of';
 export { default as formatToCurrency } from 'src/format-to-currency';
