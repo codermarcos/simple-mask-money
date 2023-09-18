49c5dea260d008542db88cb511ffc19dec7df4cc
codermarcos <coder.marcos@gmail.com>
2023-09-18 22:55:24 +1000

    🎨 improve tsdoc On branch main Changes to be committed: 	modified:   src/create-instance-of.ts 	modified:   src/format-to-number.ts

--- src/create-instance-of.ts
+++ src/create-instance-of.ts
@@ -1,11 +1,12 @@
 import type { OptionalSimpleMaskMoneyConfiguration } from 'src/types';
 
 /**
- * Will return the same function with default values.
- *
- * @param value - The first must be the function which you will use
- * @param configuration - The second should be the configuration which you will use
- * @returns A new function to use with default values
+ * It returns a the same function received but now It has default values for the configuration parameter. Allowing you to create instances of a function with different configurations without modifying the original function.
+ * It takes two parameters some function of SimpleMaskMoney and configuration
+ * 
+ * @param {<P, R>(param: P, configuration?: OptionalSimpleMaskMoneyConfiguration) => R} fn - The first must be the function which you will use
+ * @param {Partial<SimpleMaskMoneyConfiguration>} configuration - The second should be the configuration which you will use
+ * @returns {<P, R>(param: P, configuration?: OptionalSimpleMaskMoneyConfiguration) => R} A new function to use with default values
  * 
  * @example
  * Here's an example using from cdn with Vanilla JS:
@@ -23,6 +24,17 @@ import type { OptionalSimpleMaskMoneyConfiguration } from 'src/types';
  *   const remove = setMask('#my-input'); // Now this method is with the same parameters passed before
  * </script>
  * ```
+ * 
+ * @example
+ * Here's an example using from npm with ESmodules:
+ * ```javascript
+ * import { createInstanceOf, setMask } from 'simple-mask-money';
+ * const configuration = {
+ *   // Your configuration here
+ * };
+ * const setMask = createInstanceOf(setMask, configuration);
+ * const remove = setMask('#my-input'); // Now this method is with the same parameters passed before
+ * ```
  */
 function createInstanceOf<P, R>(
   fn: (param: P, configuration?: OptionalSimpleMaskMoneyConfiguration) => R,
@@ -37,3 +49,11 @@ function createInstanceOf<P, R>(
 }
 
 export default createInstanceOf;
+/**
+ * Check the {@link https://github.com/codermarcos/simple-mask-money/tree/main/examples/4.x.x#SimpleMaskMoney.createInstanceOf | SimpleMaskMoney.createInstanceOf} method to get more information about this type
+ * 
+ * @remarks
+ * This type is part of the {@link https://github.com/codermarcos/simple-mask-money/ | SimpleMaskMoney} to see the full documentation check {@link https://github.com/codermarcos/simple-mask-money/tree/main/examples/4.x.x#SimpleMaskMoney.createInstanceOf | SimpleMaskMoney.createInstanceOf}
+ * 
+ */
+export type CreateInstanceOfFunction = typeof createInstanceOf;
