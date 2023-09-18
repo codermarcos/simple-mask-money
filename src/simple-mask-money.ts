7a05e6c22aa590231e3d92b8facb57a120e2e0ba
codermarcos <coder.marcos@gmail.com>
2023-09-19 01:47:29 +1000

    🆕 export typescript types On branch main Changes to be committed: 	modified:   src/simple-mask-money.ts

--- src/simple-mask-money.ts
+++ src/simple-mask-money.ts
@@ -1,5 +1,6 @@
-export { default as setMask } from 'src/set-mask';
-export { default as removeMask } from 'src/remove-mask';
-export { default as formatToNumber } from 'src/format-to-number';
-export { default as createInstanceOf } from 'src/create-instance-of';
-export { default as formatToCurrency } from 'src/format-to-currency';
+export type { SimpleMaskMoneyConfiguration } from './types';
+export { default as setMask, SetMaskFunction } from 'src/set-mask';
+export { default as removeMask, RemoveMaskFunction } from 'src/remove-mask';
+export { default as formatToNumber, FormatToNumberFunction } from 'src/format-to-number';
+export { default as createInstanceOf, CreateInstanceOfFunction } from 'src/create-instance-of';
+export { default as formatToCurrency, FormatToCurrencyFunction } from 'src/format-to-currency';
