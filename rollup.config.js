c446bbefb5bf111c290c0ff9f1e0562ec843e6fa
codermarcos <coder.marcos@gmail.com>
2023-09-19 05:24:02 +1000

    🐛 fix rollup config for create-instace-of On branch main Changes to be committed: 	modified:   rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,38 +5,38 @@ import terser from '@rollup/plugin-terser';
 // import swc from '@rollup/plugin-swc';
 
 const getConfiguration4File = (file) => [
-    // browser-friendly UMD build
-    {
-      input: `src/${file}.ts`,
-      output: {
-        name: 'SimpleMaskMoney',
-        file: `./lib/${file}.umd.js`,
-        format: 'umd',
-        sourcemap: true,
-      },
-      plugins: [
-        resolve(), //
-        commonjs(),
-        typescript({ tsconfig: './tsconfig.build.json' }),
-        terser()
-      ],
+  // browser-friendly UMD build
+  {
+    input: `src/${file}.ts`,
+    output: {
+      name: 'SimpleMaskMoney',
+      file: `./lib/${file}.umd.js`,
+      format: 'umd',
+      sourcemap: true,
     },
-  
-    // CommonJS (for Node) and ES module (for bundlers) build.
-    {
-      input: `src/${file}.ts`,
-      output: [
-        { file: `./lib/${file}.cjs.js`, format: 'cjs', sourcemap: true },
-        { file: `./lib/${file}.esm.js`, format: 'es', sourcemap: true },
-      ],
-      plugins: [typescript({ tsconfig: './tsconfig.build.json' }), terser()],
-    },
-]
+    plugins: [
+      resolve(), //
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.build.json' }),
+      terser()
+    ],
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: `src/${file}.ts`,
+    output: [
+      { file: `./lib/${file}.cjs.js`, format: 'cjs', sourcemap: true },
+      { file: `./lib/${file}.esm.js`, format: 'es', sourcemap: true },
+    ],
+    plugins: [typescript({ tsconfig: './tsconfig.build.json' }), terser()],
+  },
+];
 
 export default [
   ...getConfiguration4File('simple-mask-money'),
   ...getConfiguration4File('set-mask'),
-  ...getConfiguration4File('create-instance'),
+  ...getConfiguration4File('create-instance-of'),
   ...getConfiguration4File('format-to-number'),
   ...getConfiguration4File('format-to-currency'),
 ];
