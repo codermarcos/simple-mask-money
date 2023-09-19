a9f5dea2a3ea3ff10369f22d6f5de27fe8a5d6da
codermarcos <coder.marcos@gmail.com>
2023-09-19 22:03:46 +1000

    🐛 fix cypress config On branch main Changes to be committed: 	modified:   cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    supportFile: false,
     baseUrl: null,
   },
 });
